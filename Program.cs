99ff9369ad6e61028673df516566f92bab8f5f21
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-18 11:10:58 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeTuningPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Optionisation  are done only when needed !, how to measure the performance!
+           // Computer Performance  is characterized by amount of the useful  work accomplished bya computer system compare 
+           // to the timeand resorces used.- KPD .Capacity of useful activity
+
+            //Performance - > Short responce for a given piece of work 
+            // good performace can mean  working bigger amount of data !
+            // GoodPerformance means --to use less computer recourses to use, less usage of Ram Memory, less usage of HardDrive Memory
+            // the performance is not only time to execute a given tasks, given commands.,  Good performance can means 
+            // less requests to the dataBase!  so less usage of the Ram Memory of the side the  DataBase/ Less usage of the Computer resources, 
+            // Less usage of the  Battery, good Performance may mean, on Bigger load the system to  continue work normally, //
+            // To have big vaule of Data and w eto be able go work. . Short data transmission time .
+            ///Balance is looked between ->performace--Quality of the code ! Not always the performance is dependantof the code quality.
+            ///sometimmes the performance is due to selecting the correct data structure !
+            ///
+            // what affects teh performance:  External devioces- > Storage, network, Internet connection , Operation System.]
+            // Code Tuning- means to write teh code or the part of teh code to be rewritten again to better the perforam nice.
+
+
+        }
+    }
+}
